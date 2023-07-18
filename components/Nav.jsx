9b9a64ad4b6c708f3f4629e92dce270bd4ff2c2f
Onyekachukwu Anyadi <sanctity2k5gmail.com>
2023-07-18 03:46:28 +0100

    fix google auth trial-seven

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -35,8 +35,6 @@ const Nav = () => {
         {/* <p className="logo_text">Gpt-Prompts</p> */}
       </Link>
 
-      {alert(providers.id)}
-
       
 
       {/* Desktop Navigation */}
@@ -66,7 +64,7 @@ const Nav = () => {
                 <button
                   type="button"
                   key={provider.name}
-                  onClick={() => signIn()}
+                  onClick={() => signIn(provider.id)}
                   className="black_btn"
                 >
                   Sign In
