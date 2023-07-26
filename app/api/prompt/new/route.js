0eaf8a4cca861081f5b836c2256f5e179cd85a8f
Onyekachukwu Anyadi <sanctity2k5gmail.com>
2023-07-26 04:41:55 +0100

    display prompts in homepage

---
+++ app/api/prompt/new/route.js
@@ -0,0 +1,42 @@
+import { connectToDB } from "@/utils/database";
+import Prompt from "@/app/models/prompt";
+
+
+
+
+export const GET = async (request) => {
+    try{
+        await connectToDB();
+        const prompts = await Prompt.find({}).populate('creator');
+        return new Response(JSON.stringify(prompts), {
+            status: 200,
+        });
+    } catch (error) {
+        return new Response("Failed to fetch prompts", {
+            status: 500,
+        });
+    }
+}
+export const POST = async (req) => {
+    const { userId, prompt, tag } = await req.json();
+
+    try {
+        await connectToDB();
+        const newPrompt = new Prompt({
+            creator: userId,
+            prompt,
+            tag  
+        });
+
+        await newPrompt.save();
+
+        return new Response (JSON.stringify(newPrompt), {
+            status: 201,
+        });
+        
+    } catch (error) {
+        return new Response("Failed to create a new prompt", {
+            status: 500,
+        });
+    }
+}
